4c7832da3ca6add05d36f39116b54a97bc1aaebf
brikhotso <brenda.rikhotso@gmail.com>
2024-02-27 16:08:52 -0800

    updated function

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -7,15 +7,17 @@
  */
 int is_full_recursive(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (1);
+	if (tree != NULL)
+	{
+		if ((tree->left != NULL && tree->right == NULL) ||
+		    (tree->left == NULL && tree->right != NULL) ||
+		    is_full_recursive(tree->left) == 0 ||
+		    is_full_recursive(tree->right) == 0)
 
-	if ((tree->left == NULL && tree->right != NULL) ||
-	    (tree->left != NULL && tree->right == NULL))
-		return (0);
+			return (0);
+	}
 
-	return (is_full_recursive(tree->left) &&
-		is_full_recursive(tree->right));
+	return (1);
 }
 
 /**
@@ -28,5 +30,5 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	return (!is_full_recursive(tree));
+	return (is_full_recursive(tree));
 }
